dbf8f71867cffb91cb8ce00f78ad9bbe34b35bf4
Lahiru Himesh Madusanka <lahiru.lahirumadusanka@gmail.com>
2017-01-31 13:04:58 +0530

    Initial Commit

---
+++ EPIC Leave form/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace EPIC_Leave_form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Dc_print = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.datecurrent = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtdep = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtsub = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txttp = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtdays = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbtype = new System.Windows.Forms.ComboBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.datereturn = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.datefrom = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // datecurrent
+            // 
+            this.datecurrent.CustomFormat = "dd/MM/yyyy";
+            this.datecurrent.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.datecurrent.Location = new System.Drawing.Point(90, 57);
+            this.datecurrent.MinDate = new System.DateTime(2016, 1, 1, 0, 0, 0, 0);
+            this.datecurrent.Name = "datecurrent";
+            this.datecurrent.Size = new System.Drawing.Size(87, 20);
+            this.datecurrent.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Department";
+            // 
+            // txtdep
+            // 
+            this.txtdep.Location = new System.Drawing.Point(90, 125);
+            this.txtdep.Name = "txtdep";
+            this.txtdep.Size = new System.Drawing.Size(167, 20);
+            this.txtdep.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Your name";
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(90, 23);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(453, 20);
+            this.txtname.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtsub);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.txttp);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtname);
+            this.groupBox1.Controls.Add(this.datecurrent);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtdep);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(562, 203);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "General information";
+            // 
+            // txtsub
+            // 
+            this.txtsub.Location = new System.Drawing.Point(90, 167);
+            this.txtsub.Name = "txtsub";
+            this.txtsub.Size = new System.Drawing.Size(453, 20);
+            this.txtsub.TabIndex = 5;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(20, 170);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(54, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Substitute";
+            // 
+            // txttp
+            // 
+            this.txttp.Location = new System.Drawing.Point(90, 91);
+            this.txttp.Name = "txttp";
+            this.txttp.Size = new System.Drawing.Size(167, 20);
+            this.txttp.TabIndex = 3;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(20, 94);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(64, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "T.P Number";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtdays);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.cmbtype);
+            this.groupBox2.Location = new System.Drawing.Point(12, 221);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(251, 100);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Leave information";
+            // 
+            // txtdays
+            // 
+            this.txtdays.Location = new System.Drawing.Point(107, 57);
+            this.txtdays.Name = "txtdays";
+            this.txtdays.Size = new System.Drawing.Size(100, 20);
+            this.txtdays.TabIndex = 7;
+            this.txtdays.Text = "1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 60);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Number of days";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Type of leave";
+            // 
+            // cmbtype
+            // 
+            this.cmbtype.FormattingEnabled = true;
+            this.cmbtype.Items.AddRange(new object[] {
+            "Casual",
+            "Medical",
+            "No pay",
+            "Other",
+            "Annual",
+            "Lieu"});
+            this.cmbtype.Location = new System.Drawing.Point(107, 23);
+            this.cmbtype.Name = "cmbtype";
+            this.cmbtype.Size = new System.Drawing.Size(121, 21);
+            this.cmbtype.TabIndex = 6;
+            this.cmbtype.Text = "Casual";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.datereturn);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.datefrom);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Location = new System.Drawing.Point(269, 221);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(305, 100);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Leave duration";
+            // 
+            // datereturn
+            // 
+            this.datereturn.CustomFormat = "dd/MM/yyyy";
+            this.datereturn.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.datereturn.Location = new System.Drawing.Point(76, 57);
+            this.datereturn.MinDate = new System.DateTime(2016, 1, 1, 0, 0, 0, 0);
+            this.datereturn.Name = "datereturn";
+            this.datereturn.Size = new System.Drawing.Size(87, 20);
+            this.datereturn.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 60);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Return";
+            // 
+            // datefrom
+            // 
+            this.datefrom.CustomFormat = "dd/MM/yyyy";
+            this.datefrom.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.datefrom.Location = new System.Drawing.Point(76, 24);
+            this.datefrom.MinDate = new System.DateTime(2016, 1, 1, 0, 0, 0, 0);
+            this.datefrom.Name = "datefrom";
+            this.datefrom.Size = new System.Drawing.Size(87, 20);
+            this.datefrom.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "From";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(464, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Print leave form";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 356);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EPIC Leave form";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Drawing.Printing.PrintDocument Dc_print;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.DateTimePicker datecurrent;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtdep;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbtype;
+        private System.Windows.Forms.TextBox txtsub;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txttp;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtdays;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DateTimePicker datereturn;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker datefrom;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ EPIC Leave form/Form1.cs
@@ -0,0 +1,193 @@
+﻿using EPIC_Leave_form.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EPIC_Leave_form
+{
+    public partial class Form1 : Form
+    {
+        class leaveInfo
+        {
+            public String date { get; set; }
+            public String name { get; set; }
+            public String department { get; set; }
+            public String typeofleave { get; set; }
+            public String days { get; set; }
+            public String from { get; set; }
+            public String returnon { get; set; }
+            public String emergcontact { get; set; }
+            public String subsname { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Dc_print.PrintPage += new System.Drawing.Printing.PrintPageEventHandler (this.printDocument1_PrintPage);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+            txtdep.AutoCompleteMode = AutoCompleteMode.Suggest;
+            txtdep.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            AutoCompleteStringCollection col = new AutoCompleteStringCollection();
+            col.Add("Research & Innovation");
+            col.Add("Quality Assurance");
+            col.Add("Finance");
+            txtdep.AutoCompleteCustomSource = col;
+
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            leaveInfo listofinfo = new leaveInfo();
+
+            listofinfo.name = txtname.Text;
+            listofinfo.date = datecurrent.Text;
+            listofinfo.emergcontact = txttp.Text;
+            listofinfo.department = txtdep.Text;
+            listofinfo.subsname = txtsub.Text;
+            listofinfo.typeofleave = cmbtype.Text;
+            listofinfo.days = txtdays.Text;
+            listofinfo.from = datefrom.Text;
+            listofinfo.returnon = datereturn.Text;
+
+            string herotext = "EPIC LANKA (PVT.) LTD.";
+            string formtxt = "32/FO/AD/10";
+            string applicationtite = "APPLICATION FOR LEAVE";
+
+
+            Font printFont = new Font ("Arial", 16, FontStyle.Regular);
+            Font printFont2 = new Font("Arial", 12, FontStyle.Regular);
+            Font printFont3 = new Font("Arial", 10, FontStyle.Regular);
+
+            //Printing the background box
+            SolidBrush graybrush = new SolidBrush(Color.LightGray);
+            Rectangle rect = new Rectangle(10, 10, 810, 70);
+
+            e.Graphics.FillRectangle(graybrush, rect);
+
+            //Printing the logo
+            e.Graphics.DrawImage(EPIC_Leave_form.Properties.Resources.logo, 25, 15,80,65);
+
+            //Printing the hero text
+            e.Graphics.DrawString(herotext, printFont, Brushes.Black, 120, 35);
+            e.Graphics.DrawString(formtxt, printFont, Brushes.Black, 670, 35);
+
+            //Application title
+            e.Graphics.DrawString(applicationtite, printFont2, Brushes.Black, 300, 85);
+
+            //Date
+            e.Graphics.DrawString("DATE: " + listofinfo.date, printFont2, Brushes.Black, 25, 135);
+
+            e.Graphics.DrawString("NAME OF APPLICANT: " + listofinfo.name, printFont2, Brushes.Black, 25, 170);
+            e.Graphics.DrawString("DEPARTMENT: " + listofinfo.department, printFont2, Brushes.Black, 325, 135);
+            e.Graphics.DrawString("TYPE OF LEAVE: " + listofinfo.typeofleave, printFont2, Brushes.Black, 25, 205);
+            e.Graphics.DrawString("NO. OF DAYS: " + listofinfo.days, printFont2, Brushes.Black, 325,205);
+
+            //Printing table
+            Pen blackPen = new Pen(Color.Black,1);
+            int x = 50;
+            int y = 245;
+            int width = 740;
+            int height = 25;
+
+            e.Graphics.DrawRectangle(blackPen, x, y, width, height);
+            e.Graphics.DrawRectangle(blackPen, x, y+25, width, height);
+
+            for (int i = 0; i < 6; i++)
+            {
+                e.Graphics.DrawLine(blackPen, x+(740/6*i),y,x+(740 / 6 * i), y+50);
+
+                switch (i)
+                {
+                    case 0:
+                        e.Graphics.DrawString("Casual", printFont2, Brushes.Black, x + (740 / 6 * i)+30, y+25);
+                        if (listofinfo.typeofleave == "Casual")
+                        {
+                            e.Graphics.DrawImage(EPIC_Leave_form.Properties.Resources.tick, x + (740 / 6 * i) + 50, y, 25, 25);
+                        }
+                        break;
+                    case 1:
+                        e.Graphics.DrawString("Medical", printFont2, Brushes.Black, x + (740 / 6 * i) + 30, y + 25);
+                        if (listofinfo.typeofleave == "Medical")
+                        {
+                            e.Graphics.DrawImage(EPIC_Leave_form.Properties.Resources.tick, x + (740 / 6 * i) + 50, y, 25, 25);
+                        }
+                        break;
+                    case 2:
+                        e.Graphics.DrawString("No Pay", printFont2, Brushes.Black, x + (740 / 6 * i) + 30, y + 25);
+                        if (listofinfo.typeofleave == "No pay")
+                        {
+                            e.Graphics.DrawImage(EPIC_Leave_form.Properties.Resources.tick, x + (740 / 6 * i) + 50, y, 25, 25);
+                        }
+                        break;
+                    case 3:
+                        e.Graphics.DrawString("Other", printFont2, Brushes.Black, x + (740 / 6 * i) + 30, y + 25);
+                        if (listofinfo.typeofleave == "Other")
+                        {
+                            e.Graphics.DrawImage(EPIC_Leave_form.Properties.Resources.tick, x + (740 / 6 * i) + 50, y, 25, 25);
+                        }
+                        break;
+                    case 4:
+                        e.Graphics.DrawString("Annual", printFont2, Brushes.Black, x + (740 / 6 * i) + 30, y + 25);
+                        if (listofinfo.typeofleave == "Annual")
+                        {
+                            e.Graphics.DrawImage(EPIC_Leave_form.Properties.Resources.tick, x + (740 / 6 * i) + 50, y, 25, 25);
+                        }
+                        break;
+                    case 5:
+                        e.Graphics.DrawString("Lieu", printFont2, Brushes.Black, x + (740 / 6 * i) + 30, y + 25);
+                        if (listofinfo.typeofleave == "Lieu")
+                        {
+                            e.Graphics.DrawImage(EPIC_Leave_form.Properties.Resources.tick, x + (740 / 6 * i) + 50, y, 25, 25);
+                        }
+
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            //Drawing 2nd rect
+            e.Graphics.DrawRectangle(blackPen, x, y+50, width, height+170);
+
+            //Left items
+            e.Graphics.DrawString("LEAVE FROM: " + listofinfo.from, printFont3, Brushes.Black, x+10, y+55);
+            e.Graphics.DrawString("EMERGENCY CONTACT NUMBER: " + listofinfo.emergcontact, printFont3, Brushes.Black, x + 10, y + 80);
+            e.Graphics.DrawString("SIGNATURE OF APPLICANT: __________________", printFont3, Brushes.Black, x + 10, y + 105);
+            e.Graphics.DrawString("SUBSTITUTES NAME: " + listofinfo.subsname, printFont3, Brushes.Black, x + 10, y + 130);
+            e.Graphics.DrawString("CHECKED BY: ________________________", printFont3, Brushes.Black, x + 10, y + 155);
+            e.Graphics.DrawString("IF NOT APPROVED REASON IN BRIEF:  ________________________________________", printFont3, Brushes.Black, x + 10, y + 185);
+            e.Graphics.DrawString("                                                                  ________________________________________", printFont3, Brushes.Black, x + 10, y + 205);
+
+            //Right items
+            e.Graphics.DrawString("RETURN ON:: " + listofinfo.returnon, printFont3, Brushes.Black, x + 350, y + 55);
+            e.Graphics.DrawString("SIGNATURE: __________________" + listofinfo.subsname, printFont3, Brushes.Black, x + 450, y + 130);
+            e.Graphics.DrawString("APPROVED BY: ________________________", printFont3, Brushes.Black, x + 350, y + 155);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            //printDocument1.DefaultPageSettings.Landscape=true;
+
+            
+            printPreviewDialog1.Document = Dc_print;
+            printPreviewDialog1.Document.DefaultPageSettings.Landscape = false;
+            printDocument1.DefaultPageSettings.PaperSize = new System.Drawing.Printing.PaperSize("A5", 580, 830);
+            //printPreviewDialog1.Document.DefaultPageSettings.PaperSize = new System.Drawing.Printing.PaperSize("A5", 580, 830);
+            printPreviewDialog1.ShowDialog();
+        }
+    }
+}
